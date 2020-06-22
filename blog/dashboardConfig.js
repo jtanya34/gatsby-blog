ade5e0a8268d81c22f146f5c8730db3a8cd1858c
Tanya Jain <tanya.jain@consulting.active.ai>
2020-06-22 14:58:39 +0530

    webhooks changes

---
+++ blog/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Bog',
+              apiId: '36b102fa-6a8a-4caf-b25b-273cc1a48b86',
+              buildHookId: '5ef06d027588dea5e884bbc3',
+              name: 'sanity-gatsby-blog',
+            },
+          ]
+        }
+      }
+    ]
+  }
